9b75b35e3847ba4f8e9b3a2b14f79dc2f4e9d13c
Owen Lekala <99358368+owenlekala@users.noreply.github.com>
2023-09-22 02:28:18 +0200

    Update get_transaction.php

--- src/get_transaction.php
+++ src/get_transaction.php
@@ -1,1 +1,24 @@
+<?php
 
+$api_key = "[YOUR API KEY]";
+$site_code = "[YOUR SITE CODE]";
+$transaction_id = isset($_GET['tref']) ? $_GET['tref'] : '';
+$base_url = "https://stagingapi.ozow.com/GetTransaction";
+
+$url = "{$base_url}?siteCode={$site_code}&transactionId={$transaction_id}";
+
+$headers = array("ApiKey" => $api_key);
+
+$options = array(
+    "http" => array(
+        "header" => "ApiKey: {$api_key}\r\n",
+        "method" => "GET"
+    )
+);
+
+$context = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+echo $response;
+
+?>
